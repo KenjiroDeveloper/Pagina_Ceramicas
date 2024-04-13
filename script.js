3ea33e2ca761f53bef29d097cfb4d52820cd93fb
Cesar Aponte <aponterivas08022004@gmail.com>
2024-04-13 17:25:50 -0500

    Culminacion de la pagina de redes bodegazo ceramico

---
+++ script.js
@@ -0,0 +1,12 @@
+function facebook(){
+    window.location.href = "https://www.facebook.com/bodegazoceramico.li?mibextid=ZbWKwL";
+}
+
+function whatsapp(){
+    window.location.href = "https://api.whatsapp.com/send?phone=573176672284&text=%F0%9F%91%8B%20%C2%A1Hola!%20Mucho%20gusto%2C%20mi%20nombre%20es%20%5BTu%20nombre%5D.%20Estoy%20interesado%2Fa%20en%20los%20productos%20del%20Bodegazo%20Cer%C3%A1mico%2C%20Por%20favor%2C%20cont%C3%A1ctame%20para%20m%C3%A1s%20informaci%C3%B3n.%20%C2%A1Espero%20tu%20respuesta";
+}
+
+function googleMaps(){
+    window.location.href = "https://www.google.com/maps/@7.9217842,-72.5019792,3a,52.3y,183.4h,80.1t/data=!3m6!1e1!3m4!1sT0KPZJ2vqWZXfKApsFP24Q!2e0!7i16384!8i8192?hl=es&entry=ttu";
+}
+
